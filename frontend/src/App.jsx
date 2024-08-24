8e629ad21d70d6642ad1d985a066db89923499a2
Anderson Lopez <52222621+TheGodFatherpte@users.noreply.github.com>
2024-08-23 21:50:19 -0500

    updated Filters and fixed some issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -47,16 +47,14 @@ const App = () => {
         <Link to="/carrito">Carrito</Link>
       </Navbar>
 
-      <div className="bg-primary w-full overflow-hidden">
-        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-          <div className={`${styles.boxWidth}`}>
+      <div className="bg-primary ">
+        <div >
             <Hero />
-          </div>
         </div>
 
-        {/*<div className="bg-primary w-full overflow-hidden">
+        <div className="bg-primary w-full overflow-hidden">
           <Productsjson products={filteredProducts} />
-        </div>*/}
+        </div>
 
         <div className={`bg-dimWhite ${styles.paddingX} ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
--- frontend/src/components/Productsjson/Products.jsx
+++ frontend/src/components/Productsjson/Products.jsx
@@ -3,51 +3,12 @@ import HeartIcon from '@mui/icons-material/Favorite';
 import HeartOutlinedIcon from '@mui/icons-material/FavoriteBorder';
 import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
 import { useCart } from '../../hooks/useCart.js';
+import { Filters } from '../Productsjson/Filters.jsx';
 import './Products.css';
 
-export function Productsjson() {  
+export function Productsjson({ products }) {
 
-  console.log('llego');
-  
-
-  const products = [
-    {
-      "id": 6,
-      "title": "Sanguche 3 quesos",
-      "description": "Disfruta de nuestra irresistible sanduche extra queso, creando una explosión de sabores que deleitará tu paladar con cada bocado.",
-      "price": 1749,
-      "discountPercentage": 11.02,
-      "rating": 4.57,
-      "stock": 83,
-      "brand": "Apple",
-      "category": "laptops",
-      "thumbnail": "https://www.semana.com/resizer/v2/6J5MO5UYBRAIHLBLEIV7ETG53Q.png?auth=0b71a935d2837d47e311e69baaec3fa95d018cfca47a46f89983531f57d9e75c&smart=true&quality=75&width=1280&height=720",
-      "images": [
-         "https://i.dummyjson.com/data/products/6/1.png",
-         "https://i.dummyjson.com/data/products/6/2.jpg",
-         "https://i.dummyjson.com/data/products/6/3.png",
-         "https://i.dummyjson.com/data/products/6/4.jpg"
-      ]
-   },
-   {
-    "id": 2,
-    "title": "Sanguche 3",
-    "description": "Disfruta de nuestra irresistible sanduche extra queso, creando una explosión de sabores que deleitará tu paladar con cada bocado.",
-    "price": 899,
-    "discountPercentage": 17.94,
-    "rating": 4.44,
-    "stock": 34,
-    "brand": "Apple",
-    "category": "smartphones",
-    "thumbnail": "https://www.semana.com/resizer/v2/6J5MO5UYBRAIHLBLEIV7ETG53Q.png?auth=0b71a935d2837d47e311e69baaec3fa95d018cfca47a46f89983531f57d9e75c&smart=true&quality=75&width=1280&height=720",
-    "images": [
-       "https://i.dummyjson.com/data/products/2/1.jpg",
-       "https://i.dummyjson.com/data/products/2/2.jpg",
-       "https://i.dummyjson.com/data/products/2/3.jpg",
-       "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
-    ]
-  },
-  ]
+  console.log('LLego Componente Products', products);
 
   const { addToCart, removeFromCart, cart } = useCart();
 
@@ -65,8 +26,9 @@ export function Productsjson() {
 
   return (
     <main className='products w-full flex justify-center items-center p-8 bg-gray-100'>
+      <Filters />
       <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 list-none p-0 m-0 rounded-lg'>
-        {products.slice(0, 10).map((product) => {
+        {products.slice(0, 3).map((product) => {
           const isProductInCart = checkProductInCart(product);
 
           return (
