2cc5096bd66b6e8f1df1b7cec4047c0302ef2b6d
MarlonVargarMendoza <marlon_vargas82172@elpoli.edu.co>
2024-10-30 13:14:19 -0500

    Edición de API de combos para mostrar descripción

--- backend/app/Http/Controllers/Product/ComboController.php
+++ backend/app/Http/Controllers/Product/ComboController.php
@@ -13,8 +13,46 @@ class ComboController extends Controller
      */
     public function index()
     {
-        return $combo = Combo::select('id', 'name', 'price', 'image', 'drinks_id', 'companions_id')
-            ->where('status', true)
-            ->get();
+        $combo = Combo::with(['product' => function ($query) {
+            $query->where('status', true)
+            ->select('id', 'name')
+
+                ->with(['ingredients' => function ($query) {
+                    $query->where('status', true)
+                        ->select('ingredients.id', 'ingredients.name');
+                }]);
+        }])
+        ->with(['drink' => function ($query) {
+            $query->where('status', true)
+            ->select('id', 'name');
+        }])
+        ->with(['companion' => function ($query) {
+            $query->where('status', true)
+            ->select('id', 'name');
+        }])
+        ->select('id', 'name', 'price', 'image', 'drinks_id', 'companions_id', 'products_id')
+        ->where('status', true)
+        ->get();
+
+        foreach ($combo as $product) {
+            $ingredientsGrouped = $product->product->ingredients->groupBy('name')->map(function ($group) {
+                $ingredient = $group->first();
+                $ingredient->amount = $group->count();
+
+                if ($ingredient->amount > 1) {
+                    $ingredient->name = "{$ingredient->name} x{$ingredient->amount}";
+                }
+
+                $ingredient->makeHidden(['pivot', 'amount']);
+                return $ingredient;
+            })->values();
+
+            $product->ingredients = $ingredientsGrouped;
+            $product->setRelation('ingredients', $ingredientsGrouped);
+            $product->makeHidden(['product', 'products_id', 'companions_id', 'drinks_id']);
+        }
+        return $combo;
+
     }
 }
+
